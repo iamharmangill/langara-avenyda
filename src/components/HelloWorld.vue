9ccf9ab3c0a84191627ad37d9c30038e5ba856cb
Eduardo Landa <eduardo.landa@outlook.com>
2019-02-01 15:14:24 -0800

    fix router

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <b-jumbotron header="Bootstrap Vue" lead="Bootstrap 4 Components for Vue.js 2" >
+  <p>For more information visit website</p>
+  <b-btn variant="primary" href="#">More Info</b-btn>
+</b-jumbotron>
+  
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+/* h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+} */
+</style>
