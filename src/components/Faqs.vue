359661547971336820002eae8497cb0acd36922a
Eduardo Landa <eduardo.landa@outlook.com>
2019-02-02 14:05:48 -0800

    Update Navbar and FAQ, Home updated

---
+++ src/components/Faqs.vue
@@ -0,0 +1,36 @@
+<template>
+<b-container class="bv-example-row">
+    <b-row>
+        <b-col>
+    <ul>
+        <li v-for="question in questions" :key="question.index">{{ question }}</li>
+    </ul>
+        </b-col>
+        <b-col>
+    <ul>
+        <li v-for="answer in answers" :key="answer.index">{{ answer }}</li>
+    </ul>
+        </b-col>
+    </b-row>
+</b-container>
+</template>
+
+<script>
+
+
+
+export default {
+  name: 'Faqs',
+  data () {
+    return {
+    questions:  ['What`s Avenyda?', 'How can I log in?', 'Can I get a refund?'],
+    answers:  ['It`s a company.', 'Ask support.', 'Contact us to solve this problem.'],
+  }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
